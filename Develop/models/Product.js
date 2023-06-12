557b7fd3d28a56a7d5f89a50c13c0ddb0ed1e35c
Dcastillomar <125750537+Dcastillomar@users.noreply.github.com>
2023-06-12 18:20:56 -0500

    add columns for Product model

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,6 +10,23 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name,
+    price,
+    stock,
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: "category",
+        key: "id"
+      }
+    }
   },
   {
     sequelize,
